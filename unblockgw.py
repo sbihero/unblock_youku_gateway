a45644e1c38b118917849824192b3396019c84d0
sbihero <sbihero@gmail.com>
2023-07-27 17:47:18 -0700

    add surge and restore function

--- unblockgw.py
+++ unblockgw.py
@@ -31,7 +31,12 @@ def main():
 """
     elogger.debug("")
     elogger.debug(" ".join(sys.argv))
-    Gateway.execute(sys.argv[1:])
+
+    if args.cmd == 'gateway':
+        Gateway.execute(sys.argv[2:])
+    elif args.cmd == 'surge':
+        Surge.execute(sys.argv[2:])
+
 
 class Gateway(object):
 
@@ -39,18 +44,19 @@ class Gateway(object):
     def execute(cls, raw_args):
         """python3 unblockgw.py {status,on,off,check,renew,setup,create}
 
-Unblock CHN 网关命令：
+Unblock Gateway 网关命令：
   status                  查看代理状态
   on                      开启代理
   off                     关闭代理
   check <URL/IP/域名>     检查 <URL/IP/域名> 是否走代理
   renew                   更新规则
   setup [--no-ss]         一键配置网关 [--no-ss: 跳过配置 ss-redir]
+  restore [--no-ss]       还原路由器为未配置状态 [--no-ss: 跳过还原 ss-redir]
   create                  仅生成 ipset 规则配置文件
 """
         parser = argparse.ArgumentParser(usage=cls.execute.__doc__)
         parser.add_argument(
-            'cmd', choices=['status', 'on', 'off', 'check', 'renew', 'setup', 'create'])
+            'cmd', choices=['status', 'on', 'off', 'check', 'renew', 'setup', 'restore', 'create'])
         args = parser.parse_args(raw_args[0:1])
 
         if args.cmd == 'create':
@@ -72,6 +78,8 @@ Unblock CHN 网关命令：
             cls.cmd_renew()
         elif args.cmd == 'setup':
             cls.cmd_setup(raw_args[1:])
+        elif args.cmd == 'restore':
+            cls.cmd_restore(raw_args[1:])
 
     @classmethod
     def cmd_status(cls):
@@ -89,7 +97,7 @@ Unblock CHN 网关命令：
 
     @classmethod
     def cmd_on(cls):
-        """开启 Unblock CHN 代理"""
+        """开启 Unblock Gateway 代理"""
         cls.check_setup()
         ss_redir_running = cls.check_ss_redir()
         if not ss_redir_running:
@@ -107,7 +115,7 @@ Unblock CHN 网关命令：
 
     @classmethod
     def cmd_off(cls):
-        """关闭 Unblock CHN 代理"""
+        """关闭 Unblock Gateway 代理"""
         cls.check_setup()
         iptables_chn_exists = cls.check_iptables_chn()
         if iptables_chn_exists:
@@ -119,7 +127,7 @@ Unblock CHN 网关命令：
     @classmethod
     def cmd_check(cls, raw_args):
         """python3 unblockgw.py router check [-h] url
-        检查 url 是否走 Unblock CHN 代理
+        检查 url 是否走 Unblock Gateway 代理
         """
         parser = argparse.ArgumentParser(usage=cls.cmd_check.__doc__)
         parser.add_argument('url', help="URL / IP / 域名")
@@ -143,7 +151,7 @@ Unblock CHN 网关命令：
 
     @classmethod
     def cmd_renew(cls):
-        """更新 Unblock CHN 规则"""
+        """更新 Unblock Gateway 规则"""
         cls.check_setup()
 
         # 生成网关配置文件
@@ -166,7 +174,7 @@ Unblock CHN 网关命令：
     @classmethod
     def cmd_setup(cls, raw_args):
         """python3 unblockgw.py router setup [-h] [--no-ss]
-        Unblock CHN 一键配置网关
+        Unblock Gateway 一键配置网关
         """
         parser = argparse.ArgumentParser(usage=cls.cmd_setup.__doc__)
         parser.add_argument('--no-ss', action='store_true', help="跳过配置 ss-redir")
@@ -181,7 +189,6 @@ Unblock CHN 网关命令：
         unblock_youku = UnblockYouku()
         cls.create_conf_files(unblock_youku.black_domains)
 
-
         # 清空 ipset 的 chn 表
         cmd = "ipset flush"
         subprocess.check_call(cmd, shell=True)
@@ -194,6 +201,73 @@ Unblock CHN 网关命令：
         cls.add_renew_cron_job()
 
         ologger.info("配置成功")
+
+@classmethod
+    def cmd_restore(cls, raw_args):
+        """python3 unblockchn.py router setup [-h] [--no-ss]
+        Unblock Gateway 还原网关为未配置状态
+        """
+        parser = argparse.ArgumentParser(usage=cls.cmd_restore.__doc__)
+        parser.add_argument('--no-ss', action='store_true', help="跳过还原 ss-redir")
+        args = parser.parse_args(raw_args)
+
+        # 不跳过还原 ss-redir
+        if not args.no_ss:
+            # 停止 ss-redir
+            ss_redir_running = cls.check_ss_redir()
+            if ss_redir_running:
+                cls.stop_ss_redir()
+
+            # 从启动脚本里移除 ss-redir 启动命令
+            comment = "# ss-redir"
+            cls.remove_from_script(SERVICES_START_SCRIPT_PATH, comment)
+            elogger.info(f"✔ 从启动脚本里移除 ss-redir 启动命令：{SERVICES_START_SCRIPT_PATH}")
+
+        # # 若 ipset 模板内有其它内容则生成对应配置文件并复制到 jffs
+        # # 否则就删除 jffs 中的配置文件
+        # ipset_has_conf = cls.create_ipset_conf_file(ipset_rules=None)
+        # if ipset_has_conf:
+        #     cls.cp_ipset_conf_to_jffs()
+        # else:
+        #     if os.path.isfile(IPSET_CONF_JFFS_PATH):
+        #         os.remove(IPSET_CONF_JFFS_PATH)
+        #         elogger.info(f"✔ 删除：{IPSET_CONF_JFFS_PATH}")
+
+        #         # 从启动脚本里移除 ipset 载入命令
+        #         comment = "# Load ipset rules"
+        #         cls.remove_from_script(NAT_START_SCRIPT_PATH, comment)
+        #         elogger.info(f"✔ 从启动脚本里移除 ipset 载入命令：{NAT_START_SCRIPT_PATH}")
+
+        # 删除 iptables 规则
+        iptables_chn_exists = cls.check_iptables_chn()
+        if iptables_chn_exists:
+            cls.delete_iptables_chn()
+            elogger.info(f"✔ 删除 iptables 规则：{DELETE_IPTABLES_CHN_CMD}")
+
+        # 从启动脚本里移除 iptables 规则添加命令
+        comment = "# Redirect chn ipset to ss-redir"
+        cls.remove_from_script(NAT_START_SCRIPT_PATH, comment)
+        elogger.info(f"✔ 从启动脚本里移除 iptables 规则添加命令：{NAT_START_SCRIPT_PATH}")
+
+        # 删除 ipset 的 chn 表
+        ipset_cmd = "ipset destroy chn"
+        try:
+            subprocess.check_output(ipset_cmd, shell=True, stderr=subprocess.PIPE)
+        except subprocess.CalledProcessError as e:
+            if "The set with the given name does not exist" not in str(e.stderr):
+                raise e
+        else:
+            elogger.info(f"✔ 删除 ipset 的 chn 表：{ipset_cmd}")
+
+        # 移除每日更新规则的 cron 定时任务
+        cls.remove_renew_cron_job()
+
+        # 从启动脚本里移除 xt_set 模块加载命令
+        comment = "# Load xt_set module"
+        cls.remove_from_script(SERVICES_START_SCRIPT_PATH, comment)
+        elogger.info(f"✔ 从启动脚本里移除 xt_set 模块加载命令：{SERVICES_START_SCRIPT_PATH}")
+
+        ologger.info("还原成功")
 
     @classmethod
     def cmd_create(cls):
@@ -583,6 +657,163 @@ class UnblockYouku(object):
                 extra_urls = f.readlines()      
         
         return extra_urls
+
+
+class Surge(object):
+
+    @classmethod
+    def execute(cls, raw_args):
+        """python3 unblockgw.py surge [-h] [-d DST]
+        Unblock Gateway
+        生成 Surge 配置文件
+        """
+        parser = argparse.ArgumentParser(usage=cls.execute.__doc__)
+        parser.add_argument('-u', '--url', action='store_true', help="生成基于 URL 正则表达式的规则（默认基于域名）")
+        parser.add_argument('-r', '--ruleset', action='store_true', help="生成 Surge ruleset 文件")
+        parser.add_argument('-d', '--dst', help="保存生成的文件到此目录")
+        args = parser.parse_args(raw_args)
+
+        unblock_youku = UnblockYouku()
+
+        if args.url:  # 基于 URL 生成规则
+            black_urls = unblock_youku.black_urls
+            white_urls = unblock_youku.white_urls
+            rules = cls.url_rules(black_urls, white_urls)
+        else:  # 基于域名生成规则
+            black_domains = unblock_youku.black_domains
+            rules = cls.domain_rules(black_domains)
+
+        if args.ruleset:  # 生成 Surge ruleset 文件
+            cls.create_ruleset_file(rules)
+        else:  # 生成 Surge 规则配置文件
+            has_conf = cls.create_conf_files(rules)
+            if not has_conf:
+                elogger.error("✘ 目录下不存在后缀为 .conf.tpl 的 Surge 配置模板文件（忽略 sample_surge.conf.tpl）")
+                sys.exit(1)
+
+        # 保存生成的文件到 args.dst
+        if args.dst:
+            if not os.path.exists(args.dst):
+                elogger.error(f"✘ 目的地文件夹不存在：{args.dst}")
+                sys.exit(1)
+            if not os.path.isdir(args.dst):
+                elogger.error(f"✘ 目的地路径非文件夹：{args.dst}")
+                sys.exit(1)
+            if args.ruleset:  # 复制 Surge ruleset 文件
+                cls.cp_ruleset_file(args.dst)
+            else:  # 复制 Surge 规则配置文件
+                cls.cp_conf_files(args.dst)
+
+    @classmethod
+    def url_rules(cls, black_urls, white_urls):
+        """生成基于 URL 正则表达式的规则"""
+        black_rules = cls.urls_to_rules(black_urls)
+        white_urls = cls.urls_to_rules(white_urls)
+        rules = {
+            'black': black_rules,
+            'white': white_urls
+        }
+        return rules
+
+    @classmethod
+    def urls_to_rules(cls, urls):
+        """将 urls 转换为 Surge 规则"""
+        rules = []
+        for url in urls:
+            if url.startswith('http://'):  # http
+                reg_url = re.escape(url)
+                reg_url = reg_url.replace("\\*", ".*")
+                reg_url = "^" + reg_url
+                rule = f"URL-REGEX,{reg_url}"
+            else:  # https
+                domain = urlsplit(url).hostname
+                if domain.startswith("*."):  # DOMAIN-SUFFIX
+                    domain = domain.replace("*.", "", 1)
+                    rule = f"AND,((DOMAIN-SUFFIX,{domain}),(DEST-PORT,443))"
+                else:  # DOMAIN
+                    rule = f"AND,((DOMAIN,{domain}),(DEST-PORT,443))"
+            rules.append(rule)
+        return rules
+
+    @classmethod
+    def domain_rules(cls, black_domains):
+        """生成基于域名的规则"""
+        black_rules = []
+        for domain in black_domains:
+            if domain.startswith("*."):  # DOMAIN-SUFFIX
+                domain = domain.replace("*.", "", 1)
+                rule = f"DOMAIN-SUFFIX,{domain}"
+            else:  # DOMAIN
+                rule = f"DOMAIN,{domain}"
+            black_rules.append(rule)
+        rules = {
+            'black': black_rules,
+            'white': []
+        }
+        return rules
+
+    @classmethod
+    def create_conf_files(cls, rules):
+        """从模板生成 Surge 规则配置文件"""
+        white_rules = rules['white']
+        white_rules = [rule + "," + "DIRECT" for rule in white_rules]
+        black_rules = rules['black']
+        black_rules = [rule + "," + SURGE_PROXY_GROUP_NAME for rule in black_rules]
+
+        rules = "\n".join(white_rules + black_rules)
+
+        has_conf = False
+
+        for name in os.listdir(SURGE_DIR_PATH):
+            if not name.endswith(".conf.tpl"):
+                continue
+            if name.startswith("._"):
+                continue
+            if name == "sample_surge.conf.tpl":  # 跳过样例模板
+                continue
+            tpl_path = os.path.join(SURGE_DIR_PATH, name)
+            with open(tpl_path, 'r', encoding='utf-8') as f:
+                tpl = f.read()
+            conf_name = name[:-4]
+            conf_path = os.path.join(SURGE_DIR_PATH, conf_name)
+            conf = tpl.format(rules=rules)
+            with open(conf_path, 'w', encoding='utf-8') as f:
+                f.write(conf)
+            has_conf = True
+            elogger.info(f"✔ 生成 Surge 配置文件（surge 目录）：{conf_name}")
+
+        return has_conf
+
+    @classmethod
+    def create_ruleset_file(cls, rules):
+        """生成 Surge ruleset 文件"""
+        rules = "\n".join(rules['black'])
+        ruleset_file_path = os.path.join(SURGE_DIR_PATH, "unblockchn.surge.ruleset")
+        with open(ruleset_file_path, 'w', encoding='utf-8') as f:
+            f.write(rules)
+        elogger.info("✔ 生成 Surge ruleset 文件（surge 目录）：unblockchn.surge.ruleset")
+
+    @classmethod
+    def cp_conf_files(cls, dst):
+        """复制目录下的 Surge 配置文件到 dst 文件夹"""
+        for name in os.listdir(SURGE_DIR_PATH):
+            if not name.endswith('.conf'):
+                continue
+            if name.startswith("._"):
+                continue
+            src_path = os.path.join(SURGE_DIR_PATH, name)
+            dst_path = os.path.join(dst, name)
+            shutil.copy2(src_path, dst_path)
+            elogger.info(f"✔ 保存 Surge 配置文件到：{dst_path}")
+
+    @classmethod
+    def cp_ruleset_file(cls, dst):
+        """复制目录下的 Surge ruleset 文件到 dst 文件夹"""
+        name = "unblockchn.surge.ruleset"
+        src_path = os.path.join(SURGE_DIR_PATH, name)
+        dst_path = os.path.join(dst, name)
+        shutil.copy2(src_path, dst_path)
+        elogger.info(f"✔ 保存 Surge ruleset 文件到：{dst_path}")
 
 
 def init_logging():
